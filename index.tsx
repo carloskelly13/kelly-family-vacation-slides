6aaa8e910914b933615cc910c6cf68eae6190c9c
Carlos Kelly <me@carlos.dev>
2023-12-23 20:25:21 -0600

    Added images.

--- index.tsx
+++ index.tsx
@@ -15,6 +15,15 @@ import {
 import introImg from "./assets/intro-img.png";
 import coastImg from "./assets/main.webp";
 import mapImg from "./assets/map.webp";
+import golfCourseImg from "./assets/IMG_0418.webp";
+import bikeImg from "./assets/IMG_0421.webp";
+import cityImg from "./assets/IMG_0424.webp";
+import kayakImg from "./assets/IMG_0427.webp";
+import lighthouseImg from "./assets/IMG_0429.webp";
+import shopsImg from "./assets/IMG_0438.webp";
+import teaImg from "./assets/IMG_0441.webp";
+import beachImg from "./assets/IMG_0447.webp";
+import flowersImg from "./assets/IMG_0455.webp";
 
 const theme: DeckProps["theme"] = {
   colors: {
@@ -95,6 +104,171 @@ const Presentation = () => (
         <Heading fontSize="h3">Where is Mackinac Island?</Heading>
         <img src={mapImg} className="h-3/4 drop-shadow-2xl" />
       </FlexBox>
+    </Slide>
+    <Slide>
+      <FlexBox height="100%" flexDirection="column">
+        <Heading fontSize="h3">Culinary Delights on Mackinac Island</Heading>
+        <UnorderedList className="list-disc w-9/12">
+          <ListItem>
+            <span className="font-bold">Diverse Dining Options:</span> From
+            upscale dining in historic hotels to casual lakeside eateries.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Local Specialties:</span> Emphasis on
+            fresh, locally-sourced ingredients, including Great Lakes fish and
+            Michigan-grown produce.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Mackinac Island Fudge:</span> A must-try
+            delicacy, with numerous fudge shops offering a variety of flavors.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Ice Cream Parlors:</span> Hand-crafted
+            ice cream, a favorite treat for families, especially during summer.
+          </ListItem>
+        </UnorderedList>
+      </FlexBox>
+    </Slide>
+    <Slide>
+      <FlexBox height="100%" flexDirection="column">
+        <Heading fontSize="h3">Afternoon Tea Tradition</Heading>
+        <UnorderedList className="list-disc w-9/12">
+          <ListItem>
+            <span className="font-bold">Elegant Experience:</span> Many historic
+            hotels on the island offer a traditional afternoon tea service,
+            complete with fine china and silverware.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Assortment of Teas:</span> A selection
+            of high-quality teas accompanied by scones, clotted cream, jam, and
+            delicate finger sandwiches.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Scenic Settings:</span> Enjoy tea with a
+            view, overlooking manicured gardens or the sparkling waters of Lake
+            Huron.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Family-Friendly:</span> A charming way
+            to introduce children to a classic tradition, while they ignore the
+            scenery and play games on their Nintendo.
+          </ListItem>
+        </UnorderedList>
+      </FlexBox>
+    </Slide>
+    <Slide>
+      <FlexBox height="100%" flexDirection="column">
+        <Heading fontSize="h3">Golfing on Mackinac Island</Heading>
+        <UnorderedList className="list-disc w-9/12">
+          <ListItem>
+            <span className="font-bold">Scenic Golf Courses:</span> The island
+            boasts picturesque golf courses that blend naturally with the
+            surrounding landscapes.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">The Jewel:</span> A unique 18-hole
+            course, with the front and back nine separated by a horse-drawn
+            carriage ride through the island’s interior.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Wawashkamo Golf Club:</span> One of the
+            oldest continuously played golf courses in Michigan, offering a
+            historic and challenging round.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Family-Friendly:</span> Mini-golf and
+            other fun recreational activities suitable for all ages.
+          </ListItem>
+        </UnorderedList>
+      </FlexBox>
+    </Slide>
+    <Slide>
+      <FlexBox height="100%" flexDirection="column">
+        <Heading fontSize="h3">Activities, Nature, and Recreation</Heading>
+        <UnorderedList className="list-disc w-9/12">
+          <ListItem>
+            <span className="font-bold">Mackinac Island State Park:</span> Over
+            70 miles of hiking and biking trails.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Horse-Drawn Carriage Tours:</span>{" "}
+            Special themed tours available, including ghost tours and historical
+            routes.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Water-Based Activities:</span> Kayaking
+            and paddleboarding along the shoreline. Guided boat tours offering
+            views of the island and surrounding areas. Pristine beaches for
+            swimming, sunbathing, and relaxation.
+          </ListItem>
+          <ListItem>
+            <span className="font-bold">Nature and Wildlife Encounters:</span>{" "}
+            Butterfly conservatories and nature centers perfect for younger
+            children. Spotting wildlife, including birds and the famous Mackinac
+            Island horses.
+          </ListItem>
+        </UnorderedList>
+      </FlexBox>
+    </Slide>
+    <Slide>
+      <div className="relative h-full w-full">
+        <img
+          src={golfCourseImg}
+          className="h-1/2 w-auto rounded-3xl drop-shadow-2xl absolute top-5 left-14 transform -rotate-1 hover:rotate-1 transition-all duration-500"
+        />
+        <img
+          src={bikeImg}
+          className="h-1/2 rounded-3xl drop-shadow-2xl absolute bottom-16 right-14 transform rotate-1 hover:-rotate-1 transition-all duration-500"
+        />
+      </div>
+    </Slide>
+    <Slide>
+      <div className="relative h-full w-full">
+        <img
+          src={cityImg}
+          className="h-1/2 w-auto rounded-3xl drop-shadow-2xl absolute bottom-16 left-14 transform -rotate-1 hover:rotate-1 transition-all duration-500"
+        />
+        <img
+          src={flowersImg}
+          className="h-[60%] rounded-3xl drop-shadow-2xl absolute top-5 right-14 transform rotate-1 hover:-rotate-1 transition-all duration-500"
+        />
+      </div>
+    </Slide>
+    <Slide>
+      <div className="relative h-full w-full">
+        <img
+          src={lighthouseImg}
+          className="h-[60%] w-auto rounded-3xl drop-shadow-2xl absolute top-5 left-14 transform -rotate-1 hover:rotate-1 transition-all duration-500"
+        />
+        <img
+          src={kayakImg}
+          className="h-1/2 rounded-3xl drop-shadow-2xl absolute bottom-16 right-14 transform rotate-1 hover:-rotate-1 transition-all duration-500"
+        />
+      </div>
+    </Slide>
+    <Slide>
+      <div className="relative h-full w-full">
+        <img
+          src={shopsImg}
+          className="h-1/2 w-auto rounded-3xl drop-shadow-2xl absolute bottom-16 left-14 transform -rotate-1 hover:rotate-1 transition-all duration-500"
+        />
+        <img
+          src={teaImg}
+          className="h-1/2 rounded-3xl drop-shadow-2xl absolute top-5 right-14 transform rotate-1 hover:-rotate-1 transition-all duration-500"
+        />
+      </div>
+    </Slide>
+    <Slide>
+      <div className="relative h-full w-full">
+        <img
+          src={beachImg}
+          className="h-[60%] w-auto rounded-3xl drop-shadow-2xl absolute top-5 left-14 transform -rotate-1 hover:rotate-1 transition-all duration-500"
+        />
+        <img
+          src={coastImg}
+          className="h-[60%] rounded-3xl drop-shadow-2xl absolute bottom-16 right-14 transform rotate-1 hover:-rotate-1 transition-all duration-500"
+        />
+      </div>
     </Slide>
     <Slide>
       <FlexBox height="100%" flexDirection="column">
